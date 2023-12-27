054806866b3ee8f00ea9e4453ac713a0563ee53f
Angelica Fallas Blanco <angelica.fallas@gmail.com>
2023-12-26 21:48:56 -0600

    data struct map,struct

--- DataStruct-Structs/main.go
+++ DataStruct-Structs/main.go
@@ -17,6 +17,17 @@ type personP struct {
 	last            string
 	iceCreamFlavors []string
 }
+type engine struct {
+	electric bool
+}
+
+type vehicle struct {
+	engine
+	make  string
+	model string
+	doors int
+	color string
+}
 
 func main() {
 	//annonymous struct
@@ -56,9 +67,60 @@ func main() {
 		iceCreamFlavors: []string{"choco", "lime"},
 	}
 
+	p4 := personP{
+		first:           "Rox",
+		last:            "Qeuesa",
+		iceCreamFlavors: []string{"Mint", "Vanilla"},
+	}
+
 	//fmt.Println(p3)
 	for i, v := range p3.iceCreamFlavors {
 		fmt.Println(i, v)
+	}
+
+	fmt.Println("-------Map of person------")
+	m := map[string]personP{
+		p3.last: p3,
+		p4.last: p4,
+	}
+
+	for k, v := range m {
+		for _, f := range v.iceCreamFlavors {
+			fmt.Println(k, f)
+		}
+	}
+
+	fmt.Println("-------Embed struct------")
+	v1 := vehicle{
+		engine: engine{
+			electric: true,
+		},
+		make:  "Toyota",
+		model: "Rav4",
+		color: "Blue",
+		doors: 4,
+	}
+
+	fmt.Println(v1.electric, v1.make, v1.model, v1.color, v1.doors)
+
+	fmt.Println("-------Anonym struct------")
+	f := struct {
+		first     string
+		friends   map[string]int
+		favDrinks []string
+	}{
+		first:     "Vero",
+		friends:   map[string]int{"Raque": 8, "Chela": 9},
+		favDrinks: []string{"water", "Coffee"},
+	}
+
+	//fmt.Println(f)
+
+	for k, v := range f.friends {
+		fmt.Println(k, v)
+	}
+	for k, v := range f.favDrinks {
+		fmt.Println(k, v)
 	}
 
 }
